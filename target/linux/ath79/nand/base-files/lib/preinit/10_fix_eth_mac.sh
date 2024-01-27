0ee01a1f2e3a89098d08548040c6c1639666edde
Micha Ober <git@ober-mail.de>
2024-01-27 20:34:39 +0100

    Use correct MAC address for eth0

--- target/linux/ath79/nand/base-files/lib/preinit/10_fix_eth_mac.sh
+++ target/linux/ath79/nand/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -3,6 +3,10 @@
 
 preinit_set_mac_address() {
 	case $(board_name) in
+	motorola,ap650)
+		ethaddr=$(mtd_get_mac_ascii config ethaddr)
+		ip link set dev eth0 address $ethaddr
+		;;
 	zyxel,emg2926-q10a|\
 	zyxel,nbg6716)
 		ethaddr=$(mtd_get_mac_ascii u-boot-env ethaddr)
